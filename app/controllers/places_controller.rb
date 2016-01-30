1aeecebc5be73479448864b1b20f3040837f8737
prakashv17 <prakashv17@gmail.com>
2016-01-30 00:07:57 +0000

    removing mediums added chapter 24

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,6 +4,7 @@ class PlacesController < ApplicationController
 	def index
 		@places = Place.all
 		@places = Place.paginate(:page => params[:page], :per_page => 5)
+		
 	end
 
 	def new
@@ -29,13 +30,17 @@ class PlacesController < ApplicationController
 		redirect_to root_path
 	end
 
+	def destroy
+		@place = Place.find(params[:id])
+		@place.destroy
+		redirect_to root_path
+	end
+
 	private
 
 	def place_params
 		params.require(:place).permit(:name, :description, :address)
 	end
-
-
 
 
 end
